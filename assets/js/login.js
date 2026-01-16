00fc5052a7921e4765ad5dc45fe0749cd7918d3c
MarcEvil <marceloinformatico.82@gmail.com>
2026-01-16 16:45:06 -0300

    actualizacion de login.html e implementacion a login.js con jquery, modificacion para sendmoney.css por problemas de diseño de formulario

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,14 +1,33 @@
-const boton = document.getElementById("btnLog");
+// const boton = document.getElementById("btnLog");
 
-boton.addEventListener('click', () => {
-    //captura de valores de inputs
-    const email = document.getElementById('email').value;
-    const pass = document.getElementById('password').value;
-    //verificar credenciales 
-    if(email === "xx@xxx.com" && pass === "1234"){
-        alert("Bienvenido a Alky Wallet");
-        window.location.href = "menu.html"
-    }else{
-        alert("Credenciales no son validas. Intente nuevamente")
-    }
+// boton.addEventListener('click', () => {
+//     //captura de valores de inputs
+//     const email = document.getElementById('email').value;
+//     const pass = document.getElementById('password').value;
+//     //verificar credenciales 
+//     if(email === "xx@xxx.com" && pass === "1234"){
+//         alert("Bienvenido a Alky Wallet");
+//         window.location.href = "menu.html"
+//     }else{
+//         alert("Credenciales no son validas. Intente nuevamente")
+//     }
+// });
+
+$(document).ready(function() {
+    // Escuchar el evento click en el botón con ID btnLog
+    $('#btnLog').on('click', function() {
+        
+        // Captura de valores de los inputs usando .val()
+        const email = $('#email').val();
+        const pass = $('#password').val();
+
+        // Verificar credenciales con una estructura condicional if-else
+        if (email === "xx@xxx.com" && pass === "1234") {
+            alert("Bienvenido a Alky Wallet");
+            // Redirección
+            window.location.href = "menu.html";
+        } else {
+            alert("Credenciales no son validas. Intente nuevamente");
+        }
+    });
 });
